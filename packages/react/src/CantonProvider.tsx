8971c2e90ec6076a400ebfc77dd18e74b82c052f
Jason Zhou <jasonzhouu@gmail.com>
2026-04-26 00:59:25 +0800

    fix(react): attach @deprecated JSDoc directly to each re-export in CantonProvider

--- packages/react/src/CantonProvider.tsx
+++ packages/react/src/CantonProvider.tsx
@@ -1,3 +1,5 @@
 /** @deprecated Use WalletProvider instead. Will be removed in a future release. */
 export { WalletProvider as CantonProvider } from './WalletProvider.js'
+
+/** @deprecated Use WalletProviderConfig / WalletProviderProps instead. Will be removed in a future release. */
 export type { WalletProviderConfig as CantonProviderConfig, WalletProviderProps as CantonProviderProps } from './WalletProvider.js'
